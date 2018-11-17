4fa9d3c5d261f2a4fff4009fd18b3e98dffbfca0
Diana T <di.tamas.dt@gmail.com>
2018-11-17 16:36:06 +0000

    changed port

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
       <App />
   </BrowserRouter>,
   document.getElementById('root'));
+unregister();
