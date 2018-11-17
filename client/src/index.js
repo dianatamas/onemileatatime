fb067b1b4c9b522d87f1f4a8ca86425af1fd832c
Diana T <di.tamas.dt@gmail.com>
2018-11-17 18:38:26 +0000

    Fixed proxy in package.json

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
       <App />
   </BrowserRouter>,
   document.getElementById('root'));
-unregister();
